4ac9e5f0392d132f88dc316d40ca87220ec05d16
Jason Scott <dev.jason.scott@gmail.com>
2018-05-03 19:44:23 +0200

    styled menu item

--- src/components/MoveListItem.js
+++ src/components/MoveListItem.js
@@ -1,7 +1,33 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import { ListGroupItem } from 'react-bootstrap';
+
+const ItemContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TitleWrapper = styled.span`
+  display: inline-flex;
+  align-items: center;
+`;
+
+const Episode = styled.span`
+  font-size: 10px;
+  font-weight: light;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  margin-right: 15px;
+`;
+
+const Title = styled.span`
+  font-weight: semi-bold;
+  font-size: 14px;
+`;
+
 
 class MovieListItem extends PureComponent {
   handleClick = () => {
@@ -11,9 +37,13 @@ class MovieListItem extends PureComponent {
     const { movie: { fields } } = this.props;
     return (
       <ListGroupItem onClick={this.handleClick}>
-        <span>EPISODE {fields.episode_id}</span>
-        <span>{fields.title}</span>
-        <span>{fields.release_date}</span>
+        <ItemContent>
+          <TitleWrapper>
+            <Episode>Episode {fields.episode_id}</Episode>
+            <Title>{fields.title}</Title>
+          </TitleWrapper>
+          <span>{fields.release_date}</span>
+        </ItemContent>
       </ListGroupItem>
 
     );
